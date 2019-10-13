709ebd51eff0cb7e9d8d9bd892f827cf64462bfc
Joshua Landron <45753071+jlandron@users.noreply.github.com>
2019-10-13 15:32:21 -0700

    Update NetworkManager.cs

--- Assets/Scripts/Networking/NetworkManager.cs
+++ Assets/Scripts/Networking/NetworkManager.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class NetworkManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private static NetworkManager _instance = null;
+    private void Awake( ) {
+        if( _instance == null ) {
+            _instance = this;
+            DontDestroyOnLoad( this );
+        } else {
+            Destroy( this );
+        }
     }
 }

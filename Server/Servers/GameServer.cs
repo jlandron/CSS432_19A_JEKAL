ff3d575acb19ddbb4e4b6142f2e1719033a36c29
Ed Abshire <thaldin@comcast.net>
2019-11-27 18:27:35 -0800

    Chat Server

--- Server/Servers/GameServer.cs
+++ Server/Servers/GameServer.cs
@@ -19,20 +19,45 @@ namespace Jekal.Servers
         public DataReciever dataReciever;
 
         //TODO: talk to Ed about integrating this code fully into his server architecture
-        public Task<int> StartServer(CancellationToken token)
+        public async Task<int> StartServer(CancellationToken token)
         {
-            throw new NotImplementedException();
+            Console.WriteLine($"GAMESERVER: Starting on {_ipAddress.ToString()}:{nPort}");
+            token.Register(serverSocket.Stop);
+
+            InitGame(); //start allowing clients to connect
+
+            while (!token.IsCancellationRequested)
+            {
+                try
+                {
+                    Thread.Sleep(100);
+                }
+                catch (ObjectDisposedException) when (token.IsCancellationRequested)
+                {
+                    Console.WriteLine("GAMESERVER: Stopping Server...");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"GAMESERVER: Error handling client connetion: {ex.Message}");
+                }
+            }
+
+            Console.WriteLine("GAMESERVER: Stopped Server...");
+            return 0;
         }
+
         public GameServer(JekalGame game)
         {
+            string serverName = Dns.GetHostName();
+            IPHostEntry hostEntry = Dns.GetHostEntry(serverName);
+            _ipAddress = Array.FindAll(hostEntry.AddressList, a => a.AddressFamily == AddressFamily.InterNetwork)[0];
             _game = game;
             nPort = Convert.ToInt32(_game.Settings["gameServerPort"]);
-            serverSocket = new TcpListener(IPAddress.Any, nPort);
+            serverSocket = new TcpListener(_ipAddress, nPort);
             gameData = new HandleGameData(this); //handles taking in the async mesages and passes them to the correct method in recieve data
             clientManager = new GameClientManager(this); //holds dictionary of each client connection
             dataSender = new DataSender(this); //responsible to sending data to clients
             dataReciever = new DataReciever(this); //responsible to recieving data from clients
-            InitGame(); //start allowing clients to connect
         }
         public void InitGame()
         {

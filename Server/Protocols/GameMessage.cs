5a60230ffd85a9ef364a402507600eb9bd32b652
Ed Abshire <thaldin@comcast.net>
2019-12-01 12:04:15 -0800

    Updates to GameMessage to handle messages.

--- Server/Protocols/GameMessage.cs
+++ Server/Protocols/GameMessage.cs
@@ -1,4 +1,5 @@
 ﻿using Jekal.Objects;
+using System.Collections.Generic;
 
 namespace Jekal.Protocols
 {
@@ -25,6 +26,9 @@ namespace Jekal.Protocols
         public int SourceId { get; set; }
         public string Target { get; set; }
         public int TargetId { get; set; }
+        public int CurrentTeamId { get; set; }
+        public int NewTeamId { get; set; }
+        public int GameTime { get; set; }
         public float PosX { get; set; }
         public float PosY { get; set; }
         public float PosZ { get; set; }
@@ -32,15 +36,21 @@ namespace Jekal.Protocols
         public float RotY { get; set; }
         public float RotZ { get; set; }
         public float RotW { get; set; }
+        public List<Player> Players { get; set; }
+        public int PlayerCount { get; set; }
 
 
         public GameMessage()
         {
             Buffer = new ByteBuffer();
+            Players = new List<Player>();
         }
 
         public bool Parse()
         {
+            // Just to be safe
+            Players.Clear();
+
             // Check to make sure we have a message
             if (Buffer.Length() < sizeof(int))
             {
@@ -58,8 +68,14 @@ namespace Jekal.Protocols
                     SourceId = Buffer.ReadInt();
                     break;
                 case Messages.TEAMJOIN:
+                    Source = Buffer.ReadString();
+                    CurrentTeamId = Buffer.ReadInt();
                     break;
                 case Messages.TEAMSWITCH:
+                    Source = Buffer.ReadString();
+                    Target = Buffer.ReadString();
+                    CurrentTeamId = Buffer.ReadInt();
+                    NewTeamId = Buffer.ReadInt();
                     break;
                 case Messages.UPDATE:
                     Source = Buffer.ReadString();
@@ -76,14 +92,32 @@ namespace Jekal.Protocols
                     Target = Buffer.ReadString();
                     break;
                 case Messages.STATUS:
+                    GameTime = Buffer.ReadInt();
+                    PlayerCount = Buffer.ReadInt();
+                    for (int i = 0; i < PlayerCount; i++)
+                    {
+                        var player = new Player();
+                        player.SessionID = Buffer.ReadInt();
+                        player.PosX = Buffer.ReadFloat();
+                        player.PosY = Buffer.ReadFloat();
+                        player.PosZ = Buffer.ReadFloat();
+                        player.RotX = Buffer.ReadFloat();
+                        player.RotY = Buffer.ReadFloat();
+                        player.RotZ = Buffer.ReadFloat();
+                        player.RotW = Buffer.ReadFloat();
+                    }
                     break;
                 case Messages.SCORE:
                     break;
                 case Messages.GAMEEND:
+                    CurrentTeamId = Buffer.ReadInt();
                     break;
                 case Messages.GAMESTART:
+                    GameTime = Buffer.ReadInt();
                     break;
                 case Messages.GAMELEAVE:
+                    Source = Buffer.ReadString();
+                    SourceId = Buffer.ReadInt();
                     break;
                 default:
                     return false;

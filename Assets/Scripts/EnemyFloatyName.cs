cd27f3243a98a588fd1749354c297b2dd65b60c7
Joshua Landron <45753071+jlandron@users.noreply.github.com>
2019-12-08 19:03:20 -0800

    reorg files and added sounds

---
+++ Assets/Scripts/EnemyFloatyName.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using NetworkGame.Client;
+
+public class EnemyFloatyName : MonoBehaviour
+{
+    [SerializeField]
+    private TextMesh playerName;
+
+    private void Start()
+    {
+        playerName.text = gameObject.name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameObject.name.Equals(playerName.text))
+        {
+            playerName.text = gameObject.name;
+        }
+    }
+}
---
+++ Assets/Scripts/RandomSFXplayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class RandomSFXplayer : MonoBehaviour {
+    AudioSource audioSource;
+    [SerializeField] AudioClip[] audioClips;
+    [SerializeField] float minTimeToWait = 10;
+    [SerializeField] float maxTimeToWait = 20;
+    void Start( ) {
+        audioSource = GetComponent<AudioSource>( );
+        StartCoroutine( RandomPlayAfterWait( ) );
+    }
+
+    IEnumerator RandomPlayAfterWait( ) {
+        while( true ) {
+            int randomSFX = Random.Range( 0, audioClips.Length );
+            audioSource.PlayOneShot( audioClips[randomSFX] );
+            yield return new WaitForSeconds( UnityEngine.Random.Range(minTimeToWait, maxTimeToWait) );
+        }
+    }
+}
